794f383ead4332fb8ca429b9a20e5bb708a119b8
JohannIngi <johannb16@ru.is>
2016-11-30 15:19:28 +0000

    breytti aðeins quotes

--- classui.cpp
+++ classui.cpp
@@ -310,9 +310,9 @@ void ClassUI::yo()
 
 string ClassUI::getQuotes()
 {
-    string quotes[3] = {"A good programmer is someone who always looks both ways before crossing a one-way street. (Doug Linder)",
-                        "Programming is like sex. One mistake and you have to support it for the rest of your life. (Michael Sinz)",
-                        "Walking on water and developing software from a specification are easy if both are frozen. (Edward V Berard)"
+    string quotes[3] = {"\"A good programmer is someone who always looks both ways before crossing a one-way street.\" (Doug Linder)",
+                        "\"Programming is like sex. One mistake and you have to support it for the rest of your life.\" (Michael Sinz)",
+                        "\"Walking on water and developing software from a specification are easy if both are frozen.\" (Edward V Berard)"
                        };
     int v1 = 0;
     srand (time(NULL));
