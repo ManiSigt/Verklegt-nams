92a62cb1e19aa07644027de9d3ab9f21387ead47
Máni Sigurðsson <manisigurdsson@Manis-MacBook-Pro-2.local>
2016-12-01 13:42:05 +0000

    litid

--- classui.h
+++ classui.h
@@ -8,7 +8,7 @@ public:
     ClassUI();
     void addPerson();           // Add a person to the database
     void mainMenu();            // This is the main menu for the program. It shows all commands to interact.
-    void remove();              // Checks if the person you wanted to delete is found then asks if you want to delete it or not, then removes it if the input was y and aborts if input was n.
+    void remove();              // Checks if the person you wanted to delete is found then asks if you want to delete it or not, then removes it if the input was y and aborts if input was anything alse.
     void run();                 // This prints out a welcome message and prints out a random quote, then calls main menu.
     void save();                // Calls the savefile function in the datalayer class.
     void search();              // Checks what option you took in searching and then asks for what keyword you want to search for, and looks it up in the database and prints it out
