7f1312a645dd8c967eac7ed5b5c75561658263f0
thorlakur16 <thorlakur16@ru.is>
2016-12-01 13:11:52 +0000

    comments 2

--- classui.h
+++ classui.h
@@ -11,15 +11,15 @@ class ClassUI
 {
 public:
     ClassUI();
-    void addPerson();   //add a person to the database
-    void mainMenu();    //this is the main menu for the program. It shows all commands to interact.
+    void addPerson();           //add a person to the database
+    void mainMenu();            //this is the main menu for the program. It shows all commands to interact.
     void remove();
     void run();
     void save();
-    void search();              // checks what option you took in searching and then asks for what keyword you want to search for, and looks it up in the database and prints it out
-    void searching();           // prints out the options to search for in search then calls search.
-    void select(string ch);
-    void sorting();             //?
+    void search();              //checks what option you took in searching and then asks for what keyword you want to search for, and looks it up in the database and prints it out
+    void searching();           //prints out the options to search for in search then calls search.
+    void select(string ch);     //selection from the menu
+    void sorting();             //sorting menu
     void view(int i);           //this prints out a single  scientist from the vector.
     void viewAll();             //This prints out the whole list of scientists, Name, Gender, Birth year, Death year and a comment.
     void yo();                  //Secret phase
