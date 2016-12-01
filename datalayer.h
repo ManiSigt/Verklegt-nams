ba4924261bcb84fb6cb2c6216ad4fd672b00b2ad
JohannIngi <johannb16@ru.is>
2016-12-01 13:32:39 +0000

    adding a few more comments

--- datalayer.h
+++ datalayer.h
@@ -16,8 +16,8 @@ public:
     void sortBirth(vector<Person>& getPersons);                                                     //Sorts the vector by year of birth.
     friend bool sortByAge(const Person &lhs, const Person &rhs);                                    //Compares two years and returns the one that is lower in chronological order.
     friend bool sortByName(const Person &lhs, const Person &rhs);                                   //Compares two names and returns the one that is lower in alphabetical order.
-    friend bool sortByGender(const Person &lhs, const Person &rhs);                                 //
-    void sortGender(vector<Person>& getPersons);                                                    //
+    friend bool sortByGender(const Person &lhs, const Person &rhs);                                 //Compares the two genders and 4just returns the one that the user wants to find.
+    void sortGender(vector<Person>& getPersons);                                                    //Sorts the vector by gender
     void sortNames(vector<Person>& getPersons);                                                     //Sorts the vector by names of people.
     int stringToNumber(string st);                                                                  //Convert string to int.
     void writeToFile(string name, char gender, int yearOfBirth, int yearOfDeath, string comment);   //Writes person to the file amazingDataBase.txt.
