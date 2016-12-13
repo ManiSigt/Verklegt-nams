d7ebc3e9cefee1983250a5b9184f9c28749cf411
Arnar Pétursson <Arnar Ptursson>
2016-12-13 14:05:18 +0000

    Hendi inni gölluðu filter

--- mainwindow.h
+++ mainwindow.h
@@ -25,6 +25,12 @@ private slots:
     void populateDropdownMenus();
     void on_dropdown_scientist_activated(const QString &arg1);
 
+    void on_input_scientist_textEdited(const QString &arg1);
+
+    void on_input_computer_textEdited(const QString &arg1);
+
+    void on_input_connections_textEdited(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
     ListWorker list;
